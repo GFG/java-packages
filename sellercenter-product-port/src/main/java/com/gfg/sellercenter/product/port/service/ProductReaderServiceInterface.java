6964a36690c8c4b33f17606c2d768572785c02ea
Masrur Mirboboev <masrur.mirboboev@global-fashion-group.com>
2020-04-23 09:23:33 +0200

    make interface public in product port

--- sellercenter-product-port/src/main/java/com/gfg/sellercenter/product/port/service/ProductReaderServiceInterface.java
+++ sellercenter-product-port/src/main/java/com/gfg/sellercenter/product/port/service/ProductReaderServiceInterface.java
@@ -2,11 +2,10 @@ package com.gfg.sellercenter.product.port.service;
 
 import com.gfg.sellercenter.product.port.entity.Product;
 
-import java.util.List;
 import java.util.Map;
 import java.io.IOException;
 
-interface ProductReaderServiceInterface {
+public interface ProductReaderServiceInterface {
     public Product getById(int productId) throws IOException;
 
     public Map<Integer, Product> getProducts(int[] productIds) throws IOException;
