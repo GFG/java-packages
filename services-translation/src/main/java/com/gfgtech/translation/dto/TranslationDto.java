d8e33f2164f8e506b95141cf65aea2b551abcb63
claudiudetesan <78016069+claudiudetesan@users.noreply.github.com>
2022-05-25 13:23:21 +0300

    SC-1486 Changed translation response to match the new contract (#61)
    Co-authored-by: c.detesan <claudiu.detesan@pitechnologies.ro>

--- services-translation/src/main/java/com/gfgtech/translation/dto/TranslationDto.java
+++ services-translation/src/main/java/com/gfgtech/translation/dto/TranslationDto.java
@@ -1,10 +1,8 @@
 package com.gfgtech.translation.dto;
 
-import java.util.List;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.jackson.Jacksonized;
-import org.springframework.util.CollectionUtils;
 
 /** @author Claudiu MAN */
 @Getter
@@ -12,9 +10,10 @@ import org.springframework.util.CollectionUtils;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TranslationDto {
-    List<TranslationPayload> payload;
 
-    public boolean isTranslationPayloadEmpty() {
-        return CollectionUtils.isEmpty(payload);
-    }
+    String language;
+
+    String key;
+
+    String value;
 }
