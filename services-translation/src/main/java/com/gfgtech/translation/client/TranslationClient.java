97bbcda3eb4ef996cd08031c8dd481b0ba228465
c.detesan <claudiu.detesan@pitechnologies.ro>
2022-05-25 12:42:47 +0300

    SC-1486 Changed translation response to match the new contract

--- services-translation/src/main/java/com/gfgtech/translation/client/TranslationClient.java
+++ services-translation/src/main/java/com/gfgtech/translation/client/TranslationClient.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 /** @author Claudiu MAN */
 @FeignClient(value = "translationClient", url = "${app.translation.base-url}", primary = false)
 public interface TranslationClient {
 
     @GetMapping(value = "${app.translation.get-translation-url}")
-    TranslationDto getTranslationByLanguageAndNamespace(
+    List<TranslationDto> getTranslationByLanguageAndNamespace(
             @PathVariable("language") String language,
             @RequestParam("namespace") String namespace,
             @RequestParam(name = "fallback", required = false) boolean fallback);
